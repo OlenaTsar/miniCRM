d72e3de77a5a362a99d2762209c23ee613e88285
OlenaTsar <olenka152017@gmail.com>
2026-06-03 00:15:53 +0300

    contact and company models

--- CRM/crm_app/models.py
+++ CRM/crm_app/models.py
@@ -1,3 +1,66 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+from auth_app.models import User
+
+
+class Company(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255, null=False, blank=False)
+    description = models.TextField(null=True, blank=True)
+    industry = models.CharField(max_length=255, null=True, blank=True)
+    website = models.URLField(blank=True, null=True)
+    email = models.EmailField(null=True, blank=True)
+    instagram_url = models.URLField(blank=True, null=True)
+    facebook_url = models.URLField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class ContactStatus(models.TextChoices):
+    LEAD = "lead", "Lead"
+    QUALIFIED = "qualified", "Qualified"
+    PROSPECT = "prospect", "Prospect"
+    ACTIVE = "active", "Active"
+    DORMANT = "dormant", "Dormant"
+    LOST = "lost", "Lost"
+
+
+class LeadSource(models.TextChoices):
+    INSTAGRAM = "instagram", "Instagram"
+    FACEBOOK = "facebook", "Facebook"
+    GOOGLE_ADS = "google_ads", "Google Ads"
+    LINKEDIN = "linkedin", "LinkedIn"
+    REFERRAL = "referral", "Referral"
+    WEBSITE = "website", "Website"
+    COLD_CALL = "cold_call", "Cold Call"
+    ORGANIC_SEARCH = "organic_search", "Organic Search"
+
+
+class Contact(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(null=True, blank=True)
+    phone = models.CharField(max_length=20)
+    instagram_url = models.URLField(blank=True, null=True)
+    facebook_url = models.URLField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    status = models.CharField(max_length=20, choices=ContactStatus.choices, default=ContactStatus.LEAD)
+    lead_source = models.CharField(max_length=20, choices=LeadSource.choices)
+    company = models.ForeignKey(
+        Company,
+        on_delete=models.SET_NULL,
+        default=None,
+        null=True,
+        blank=True,
+        related_name='contacts',
+    )
+    users = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        default=None,
+        null=True,
+        blank=True,
+        related_name='contacts',
+    )
